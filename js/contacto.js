d8f4a2a7ae6e0cc5954ce07f8a0f8a8ba4022069
Fede Villarreal <fedevillarreal94@gmail.com>
2023-01-24 18:03:58 -0300

    modifique la pagina de contacto, quitando el formulario y agregando las vías de comunicación

---
+++ js/contacto.js
@@ -0,0 +1,39 @@
+const containerWhats = document.querySelector(".containerWhats")
+const containerInsta = document.querySelector(".containerInsta")
+const containerFace = document.querySelector(".containerFace")
+const textoWhats = document.querySelector(".textoWhats")
+const textoInsta = document.querySelector(".textoInsta")
+const textoFace = document.querySelector(".textoFace")
+
+console.log(containerWhats)
+console.log(textoWhats)
+
+function mostrarWhats() {
+    containerWhats.onmouseover = () => {
+        textoWhats.style.display = "inline"
+    }
+    containerWhats.onmouseout = () => {
+        textoWhats.style.display = "none"
+    }
+}
+mostrarWhats()
+
+function mostrarInsta() {
+    containerInsta.onmouseover = () => {
+        textoInsta.style.display = "inline"
+    }
+    containerInsta.onmouseout = () => {
+        textoInsta.style.display = "none"
+    }
+}
+mostrarInsta()
+
+function mostrarFace() {
+    containerFace.onmouseover = () => {
+        textoFace.style.display = "inline"
+    }
+    containerFace.onmouseout = () => {
+        textoFace.style.display = "none"
+    }
+}
+mostrarFace()
