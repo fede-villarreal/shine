e6f0481ab5bcf245ec199aa09c8000d31346828c
Fede Villarreal <fedevillarreal94@gmail.com>
2023-01-25 17:46:31 -0300

    agregue un media query para que el texto de redes sociales se oculte y se muestre con un evento onmouseover a partir de la resolucion de pantalla 961px de ancho, quite la etiqueta anchor de las redes y cree una funcion para que al hacer clic en el texto se abra el enlace de la red demorandolo un breve lapso para permitir la animacion en dispositivos pequeños

--- js/contacto.js
+++ js/contacto.js
@@ -4,34 +4,70 @@ const containerFace = document.querySelector(".containerFace")
 const textoWhats = document.querySelector(".textoWhats")
 const textoInsta = document.querySelector(".textoInsta")
 const textoFace = document.querySelector(".textoFace")
-const linkWhats = document.querySelector(".linkWhats")
+
+const pantalla = screen.width
+
+
+function demorarLinkWhats() {
+    textoWhats.onclick = () => {
+        setTimeout ( () => {
+            window.open("https://wa.me/5493512252356")
+        }, 750 )
+    }
+}
+demorarLinkWhats()
+
+function demorarLinkInsta() {
+    textoInsta.onclick = () => {
+        setTimeout ( () => {
+            window.open("https://www.instagram.com/shine.bycb/")
+        }, 750 )
+    }
+}
+demorarLinkInsta()
+
+function demorarLinkFace() {
+    textoFace.onclick = () => {
+        setTimeout ( () => {
+            window.open("https://www.facebook.com/ShineDanzaBycb")
+        }, 750 )
+    }
+}
+demorarLinkFace()
+
 
 function mostrarWhats() {
-    containerWhats.onmouseover = () => {
-        textoWhats.style.display = "inline"
-    }
-    containerWhats.onmouseout = () => {
-        textoWhats.style.display = "none"
+    if ( pantalla >= 961 ) {
+        containerWhats.onmouseover = () => {
+            textoWhats.style.display = "inline"
+        }
+        containerWhats.onmouseout = () => {
+            textoWhats.style.display = "none"
+        }
     }
 }
 mostrarWhats()
 
 function mostrarInsta() {
-    containerInsta.onmouseover = () => {
-        textoInsta.style.display = "inline"
-    }
-    containerInsta.onmouseout = () => {
-        textoInsta.style.display = "none"
+    if ( pantalla >= 961 ) {
+        containerInsta.onmouseover = () => {
+            textoInsta.style.display = "inline"
+        }
+        containerInsta.onmouseout = () => {
+            textoInsta.style.display = "none"
+        }
     }
 }
 mostrarInsta()
 
 function mostrarFace() {
-    containerFace.onmouseover = () => {
-        textoFace.style.display = "inline"
-    }
-    containerFace.onmouseout = () => {
-        textoFace.style.display = "none"
+    if ( pantalla >= 961 ) {
+        containerFace.onmouseover = () => {
+            textoFace.style.display = "inline"
+        }
+        containerFace.onmouseout = () => {
+            textoFace.style.display = "none"
+        }
     }
 }
 mostrarFace()
