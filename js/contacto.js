afa3535bf72b9518f4c45741020524bafd566888
Fede Villarreal <fedevillarreal94@gmail.com>
2023-01-24 21:06:50 -0300

    reverti la prueba

--- js/contacto.js
+++ js/contacto.js
@@ -5,11 +5,6 @@ const textoWhats = document.querySelector(".textoWhats")
 const textoInsta = document.querySelector(".textoInsta")
 const textoFace = document.querySelector(".textoFace")
 const linkWhats = document.querySelector(".linkWhats")
-linkWhats.onclick = () => {
-    setTimeout ( () => {
-        window.open ( "https://wa.me/5493512252356", "_blank" )
-    }, 1000 )
-}
 
 function mostrarWhats() {
     containerWhats.onmouseover = () => {
